a36d7b92250fc54eeee6a5ddf29ee0323aec7cb1
douglasmonteles <silvadouglas328@gmail.com>
2023-07-24 22:38:18 -0300

    :sparkles: (#1): Creating projects page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
 
+  
+    ProjectsComponent
   ],
   imports: [
     CommonModule,
