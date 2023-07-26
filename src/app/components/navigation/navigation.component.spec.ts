1affefae7a932b9d6b70e43dfd7090d17bd4d32a
douglasmonteles <silvadouglas328@gmail.com>
2023-07-25 22:59:44 -0300

    :white_check_mark: (#1): Adding navigation tests

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -6,9 +6,16 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { RouterModule } from '@angular/router';
+import { Observable, of } from 'rxjs';
 import { FooterComponent } from '../footer/footer.component';
 import { NavigationComponent } from './navigation.component';
+
+// Criar um mock para o BreakpointObserver usando um valor fixo para isHandset$
+const mockBreakpointObserver = {
+  observe: (): Observable<any> => of({ matches: true }), // Valor fixo para isHandset$
+};
 
 describe('NavigationComponent', () => {
   let component: NavigationComponent;
@@ -28,7 +35,13 @@ describe('NavigationComponent', () => {
         MatSidenavModule,
         MatToolbarModule,
         RouterModule,
-      ]
+      ],
+      providers: [
+        {
+          provide: BreakpointObserver,
+          useValue: mockBreakpointObserver
+        }
+      ],
     }).compileComponents();
   }));
 
@@ -40,5 +53,39 @@ describe('NavigationComponent', () => {
 
   it('should compile', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render with "role=dialog" when isHandset$ is true', () => {
+    // Verificar se o valor do atributo 'role' é igual a 'dialog' quando isHandset$ é true
+    const element: HTMLElement = fixture.nativeElement;
+    const dialogElement = element.querySelector('[role="dialog"]');
+    expect(dialogElement).toBeTruthy();
+  });
+
+  it('should render with "role=navigation" when isHandset$ is false', () => {
+    // Atualizar o valor de isHandset$ para false (um cenário em que não é dispositivo móvel)
+    const breakpointObserver = TestBed.inject(BreakpointObserver) as any;
+    (breakpointObserver).activePropertyChanged = of();
+
+    // Disparar a detecção de alterações para atualizar a renderização com o novo valor de isHandset$
+    fixture.detectChanges();
+
+    // Verificar se o valor do atributo 'role' é igual a 'navigation' quando isHandset$ é false
+    const element: HTMLElement = fixture.nativeElement;
+    const navigationElement = element.querySelector('[role="navigation"]');
+    expect(navigationElement).toBeTruthy();
+  });
+
+  it('should show the mat-toolbar description', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const matToolbarText = element.querySelector('mat-toolbar')?.textContent;
+    expect(matToolbarText).toEqual('Acesso rápido');
+  });
+
+  it('should show the side-nav-list with the available menu options', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    const menuOptions = element.querySelector('mat-nav-list');
+    expect(menuOptions?.textContent).not.toBeNull();
+    expect(menuOptions?.children.length).toBeGreaterThan(0);
   });
 });
