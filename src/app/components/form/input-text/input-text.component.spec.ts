48b0285fa4ade3505211381ab06737409dc2aa35
douglasmonteles <silvadouglas328@gmail.com>
2023-08-04 23:24:00 -0300

    ✅: Testing the input-text component

--- src/app/components/form/input-text/input-text.component.spec.ts
+++ src/app/components/form/input-text/input-text.component.spec.ts
@@ -7,6 +7,13 @@ describe('InputTextComponent', () => {
   let component: InputTextComponent;
   let fixture: ComponentFixture<InputTextComponent>;
 
+  const inputMockData = {
+    label: 'Nome do usuário',
+    value: 'Douglas Silva',
+    readonly: true,
+    hint: 'Preencha com o nome do usuário',
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [InputTextComponent],
@@ -19,5 +26,26 @@ describe('InputTextComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the correctly input data', () => {
+    component.label = inputMockData.label;
+    component.value = inputMockData.value;
+    component.readonly = inputMockData.readonly;
+    component.hint = inputMockData.hint;
+    fixture.detectChanges();
+
+    const element: Element = fixture.nativeElement;
+    expect(element).not.toBeNull();
+
+    const inputLabel: Element | null = element.querySelector('mat-form-field mat-label');
+    expect(inputLabel?.textContent?.trim()).toEqual(inputMockData.label);
+
+    const inputElement: HTMLInputElement | null = element.querySelector('mat-form-field input');
+    expect(inputElement?.value).toEqual(inputMockData.value);
+    expect(inputElement?.readOnly).toBeTrue();
+
+    const inputHint: Element | null = element.querySelector('mat-form-field mat-hint');
+    expect(inputHint?.textContent?.trim()).toEqual(inputMockData.hint);
   });
 });
